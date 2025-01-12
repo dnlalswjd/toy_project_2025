36ea0e44deeadfb538e00cc50ae0f6dde99ab0fc
Minjeong Wi <wmj@khu.ac.kr>
2025-01-13 03:37:04 +0900

    내가 팔로우를 요청한 계정을 확인하는 기능 추가

--- toy_project.py
+++ toy_project.py
@@ -6,6 +6,9 @@ with open('following.json', 'r') as file:
 with open('followers_1.json', 'r') as file:
     followersFile = json.load(file)
 
+with open('pending_follow_requests.json', 'r') as file:
+    requestFile = json.load(file)
+
 following = []
 for value in followingFile.values():
     for followingData in value:
@@ -14,6 +17,11 @@ for value in followingFile.values():
 followers = []
 for followersData in followersFile:
     followers.append(followersData["string_list_data"][0]["value"])
+
+request = [] #내가 팔로우를 요청한, 남의 비공개 계정
+for value in requestFile.values():
+    for requestData in value:
+        request.append(requestData["string_list_data"][0]["value"])
 
 result1 = [] #나만 상대를 팔로우하는 계정
 for user in following:
@@ -25,14 +33,17 @@ for user in followers:
     if user not in following:
         result2.append(user)
 
-print("[내 계정의 팔로잉 수: {}]".format(len(following)))
-print("[나만 상대를 팔로우하는 계정 수: {}]".format(len(result1)))
+print("[내 계정의 팔로잉 수: {} / 나만 상대를 팔로우하는 계정 수: {}]".format(len(following), len(result1)))
 if len(result1):
     for user in result1:
         print(user)
 
-print("[내 계정의 팔로워 수: {}]".format(len(followers)))
-print("[상대만 나를 팔로우하는 계정 수: {}]".format(len(result2)))
+print("[내 계정의 팔로워 수: {} / 상대만 나를 팔로우하는 계정 수: {}]]".format(len(followers), len(result2)))
 if len(result2):
     for user in result2:
+        print(user)
+
+print("[내가 팔로우를 요청한 비공계 계정 수: {}]".format(len(request)))
+if len(request):
+    for user in request:
         print(user)
